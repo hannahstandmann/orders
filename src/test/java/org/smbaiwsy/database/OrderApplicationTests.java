cfb034c23a94c7d3c6a153f5ae5abaa6e0f84770
anamattuzzi-stojanovic <ana.mattuzzi@me.com>
2018-12-25 20:12:01 +0100

    added some repository tests

--- src/test/java/org/smbaiwsy/database/OrderApplicationTests.java
+++ src/test/java/org/smbaiwsy/database/OrderApplicationTests.java
@@ -2,12 +2,15 @@ package org.smbaiwsy.database;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.smbaiwsy.database.entity.Customer;
+import org.smbaiwsy.database.entity.Product;
 import org.smbaiwsy.database.repository.CustomerRepository;
+import org.smbaiwsy.database.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
@@ -19,7 +22,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-//@SpringBootTest
 @ComponentScan(basePackages = { "org.smbaiwsy.database" })
 @TestPropertySource(locations = "classpath:application-test.properties")
 @AutoConfigureTestDatabase(replace = Replace.ANY)
@@ -30,9 +32,12 @@ public class OrderApplicationTests {
 	}
 	@Autowired
     private CustomerRepository customers;
+	
+	@Autowired
+    private ProductRepository products;
 
     @Test
-    public void testFindByEMail() {
+    public void testFindCustomerByEMail() {
         Customer customer = new Customer(UUID.randomUUID().toString(),"ana.mattuzzi@me.com","Mihajla Pupina 103/3", "password");
         customers.save(customer);
 
@@ -40,5 +45,27 @@ public class OrderApplicationTests {
 
         assertThat(findByEmail).extracting(Customer::getEMail).isEqualTo(customer.getEMail());
     }
+    
+    @Test
+    public void testFindCustomerByReferenceId() {
+    	    
+        Customer customer = new Customer(UUID.randomUUID().toString(),"ana.mattuzzi@me.com","Mihajla Pupina 103/3", "password");
+        customers.save(customer);
+
+        Customer findByRefId = customers.findByRefId(customer.getRefId()).get();
+
+        assertThat(findByRefId).extracting(Customer::getRefId).isEqualTo(customer.getRefId());
+    }
+    
+    @Test
+    public void testFindProductByReferenceId() {
+        Product product = new Product(UUID.randomUUID().toString(),"O Bag",BigDecimal.valueOf(13000));
+        products.save(product);
+
+        Product findByRefId = products.findByRefId(product.getRefId()).get();
+
+        assertThat(findByRefId).extracting(Product::getRefId).isEqualTo(product.getRefId());
+    }
+    
 }
 
